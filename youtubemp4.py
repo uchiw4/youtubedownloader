f423e9346b5414418d351df4d32389f3269cab81
uchiw4 <77907223+uchiw4@users.noreply.github.com>
2021-07-20 11:17:46 +0200

    youtubemp4 v2
    Vous pouvez désormais choisir là ou vous désirez installer votre fichier mp4.

--- youtubemp4.py
+++ youtubemp4.py
@@ -1,43 +1,62 @@
-from tkinter import StringVar
-import pytube
+from tkinter import StringVar, Label
+from pytube import YouTube
 from tkinter import *
+from tkinter import filedialog
+
+
 
 
 def download_video():
     url = (lien.get())
-
     try:
-        youtube =  pytube.YouTube(url)
+        print(url)
+        print("0")
+        youtube =YouTube(url)
+        print("1")
         video = youtube.streams.get_highest_resolution()
-        video.download(r"\pythonProject1\Vidéos")
-        notif.config(fg="green",text="Video téléchargée BG",bg="#8440A8")
+        print("2")
+        video.download(filedialog.asksaveasfilename())
+        print("3")
+        notif.config(fg="green",text="Video downloaded !",bg="black")
+        print("4")
         notif.pack()
+
+
     except Exception as e:
         print(e)
-        notif.config(fg="red",text="Erreur lors du téléchargement de la vidéo !",bg ="#8440A8" )
+        notif.config(fg="red",text="Error while downloading the video !",bg ="black" )
         notif.pack()
+
+
+
 
 
 #afficher la fenêtre
 window = Tk()
+photo1 = PhotoImage(file = f"\Algerie.png")
 window.title("JL Youtube.mp4")
-window.iconbitmap('algeria.ico')
 window.geometry("500x500")
-window.config(background="black")
+window.iconphoto(False, photo1)
+window.config(background= "black")
 window.minsize(500, 100)
 window.maxsize(500, 500)
 
 #petite frame en brrr
-frame = Frame(window, bg="#8440A8")
+frame = Frame(window, bg="black")
+
+
 
 #création de la variable du lien
 lien = StringVar()
 
+place = StringVar()
+
 #afficher un texte
+
 Label_lien = Label(frame, text="Past the link and download !", font=("Full Pack 2025",10), bg="black", fg="white")
 Label_lien.pack()
 
-notif = Label(frame, font=("Calibri",15))
+notif = Label(frame, font=("Calibri",15), bg ="black")
 
 
 
@@ -47,6 +66,7 @@ frame_2 = Frame(window, bg="black")
 # Créez un champ
 videolink_entry = Entry(frame_2, font=("Calibri",15), textvariable = lien )
 videolink_entry.pack(fill=X)
+
 
 #petitbouton
 boutontelecharger = Button(frame_2, text="Download", font=("Full Pack 2025", 10), bg="black", fg="white", command=download_video)
@@ -60,8 +80,11 @@ frame.grid(padx=1,row=0)
 frame.pack(expand=YES)
 frame_2.pack(expand=YES)
 
-#affiche la fenetre c
+#affiche la fenetre
 window.mainloop()
+
+
+
 
 
 
